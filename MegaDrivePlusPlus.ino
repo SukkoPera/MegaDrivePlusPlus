8926a7e25aa2e6b39b6e55615cdddedd02098ab5
Giorgioggì <software@sukkology.net>
2015-09-29 00:00:09 +0200

    Simplify initial comment, linking to GitHub page

--- MegaDrivePlusPlus.ino
+++ MegaDrivePlusPlus.ino
@@ -17,54 +17,13 @@
  * along with MegaDrive++. If not, see <http://www.gnu.org/licenses/>.         *
  *******************************************************************************
  *
- * IGR, 50/60 Hz and Language Switchless mod for Sega Mega Drive (AKA Genesis)
- * By SukkoPera <software@sukkology.net> 21/08/2015
- *
- * Features:
- * - Reset-From-Pad (AKA In-Game-Reset AKA IGR): Press Start + A + B + C.
- * - Supports consoles with both active-high and active-low reset signals by
- *   autosensing. Only the former was tested, though :).
- * - EUR/USA/JAP mode switching:
- *   - Through Reset button: Keep pushed to cycle through modes.
- *   - From pad: Press Start + B + Left/Right to cycle through modes or Start +
- *     Down/Left/Right to set a certain mode.
- *   - The last used mode is saved after 10 seconds and reused at power up.
- * - Even though default settings are recommended, everything can be customized
- *   to taste.
- * - Supports common-anode or common-cathode, dual or RGB LEDs to indicate the
- *   current mode (Colors can be set to any value when PWM pins are available).
- * - Can also indicate the current mode by flashing a single LED, if you don't
- *   feel like replacing the original LED.
- * - Uses cheap Atmel AVR microcontrollers.
- * - Can be flashed on different chips (ATtiny's, ATmega's, or even a full
- *   Arduino board). Not all features are supported on all chips, depending on
- *   the number of available I/O pins, please read on for details.
- * - If flashed on an ATtiny84, it is pin-to-pin compatibile with the D4s/Seb
- *   mod.
- *
- * A description of the Mega Drive controller protocol can be found at the
- * following pages:
- * - http://www.cs.cmu.edu/~chuck/infopg/segasix.txt
- * - http://mdpal60.net/wiki/megadrive/regionmod/start
- *
- * If you have to buy a new chip from scratch I'd strongly recommend getting an
- * Arduino Nano clone, because:
- * - It is cheap (2-3 EUR straight from China).
- * - It has plenty of pins, which means it supports all the features.
- * - It has an embedded USB port and serial converter, allowing for easy
- *   upgrades (and debugging, in case).
- *
- * An ATtiny861 is also a valid choice, if you prefer sticking to a single chip,
- * as it has plenty of I/O pins.
- *
- * All diagrams below for ATtiny chips are based on ATTinyCore:
- * https://github.com/SpenceKonde/ATTinyCore
- *
- * Probably other cores will work as well, but you might have to adjust pin
- * numbers and #defines.
- *
- * NOTE: In all diagrams, outer pin number are referred to the physical chips,
- * while inner pin numbers are Arduino pin numbers.
+ * MegaDrive++ - Universal Region mod, 50/60 Hz switch and In-Game-Reset (IGR)
+ * for Sega Mega Drive (AKA Genesis) 
+ * 
+ * Copyright (C) 2015bBy SukkoPera <software@sukkology.net>
+ * 
+ * Please refer to the GitHub page and wiki for any information:
+ * https://github.com/SukkoPera/MegaDrivePlusPlus
  */
 
 // For some reason, this is needed here or compilation fails :/
