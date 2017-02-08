c964ee3b4b54749c9887ac606ebe7b73e32529ef
SukkoPera <software@sukkology.net>
2017-02-08 23:14:46 +0100

    Update copyright notice

--- MegaDrivePlusPlus.ino
+++ MegaDrivePlusPlus.ino
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of MegaDrive++.                                           *
  *                                                                             *
- * Copyright (C) 2015-2016 by SukkoPera <software@sukkology.net>               *
+ * Copyright (C) 2015-2017 by SukkoPera <software@sukkology.net>               *
  *                                                                             *
  * MegaDrive++ is free software: you can redistribute it and/or modify         *
  * it under the terms of the GNU General Public License as published by        *
